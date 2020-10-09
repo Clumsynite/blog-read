141394b0df3e1d23030d02f0b318aed9fd2a8014
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-09 14:53:58 +0530

    updated to add navbar as component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+class App extends Component {
+  constructor() {
+    super();
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    this.state = {
+      blogs: [],
+      user: {},
+      comments: [],
+      authenticated: false,
+    };
+  }
+  render() {
+    const { authenticated, blog, user, comments } = this.state;
+    return (
+      <div className="App">
+        <Navbar authenticated={authenticated} />
+      </div>
+    );
+  }
 }
 
 export default App;
