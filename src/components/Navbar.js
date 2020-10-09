f05820512c4e912c432e64b18d8248de137bb6c8
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-09 19:05:47 +0530

    added margin-bottom on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ class Navbar extends Component {
     const { authenticated } = this.props;
     return (
       <div className="Navbar">
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-2">
           <div className="container">
             <a className="navbar-brand" href="#">
               Clumsyknight's Blog
