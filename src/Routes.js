7e25231586cfe6359cd337491ff09dc9447f9d79
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-27 16:18:17 +0530

    improved routes added container

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,29 @@
-import { HashRouter, Switch, Link, Redirect } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
 
 function Routes() {
+  const [blogs, setblogs] = useState([]);
+  const [user, setuser] = useState({});
+  const [comments, setcomments] = useState([]);
+  const [authenticated, setauthenticated] = useState(false);
+
   return (
     <div className="Routes">
-      <h1>Routes</h1>
+      <Router>
+        <Navbar authenticated={authenticated} />
+        <div className="container">
+          <Switch>
+            <Route path="/login" component={Login} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
