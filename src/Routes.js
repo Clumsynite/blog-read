760406677f143edd00a69b5de94707aad9bd847f
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-27 15:06:22 +0530

    changed App to Routes

---
+++ src/Routes.js
@@ -0,0 +1,11 @@
+import { HashRouter, Switch, Link, Redirect } from "react-router-dom";
+
+function Routes() {
+  return (
+    <div className="Routes">
+      <h1>Routes</h1>
+    </div>
+  );
+}
+
+export default Routes;
