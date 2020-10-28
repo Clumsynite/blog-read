f1c044ccb1462e89746b3e6d226c1d4fb6fd4562
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-28 19:28:00 +0530

    trying to get all blogs

--- src/components/AllBlogs.js
+++ src/components/AllBlogs.js
@@ -2,11 +2,29 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const AllBlogs = () => {
+  const allBlogsUrl = "https://clumsy-blog.herokuapp.com/blog/all";
   const [blogs, setblogs] = useState([]);
-  useEffect(() => {});
+
+  const getAllBlogs = async () => {
+    try {
+      const response = await axios.get(allBlogsUrl);
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.log(error.response);
+      return error;
+    }
+  };
+  const handleClick = async () => {
+    const blogs = await getAllBlogs();
+    console.log(blogs);
+  };
   return (
     <div className="Blog">
       <h1 className="text-center">Blog</h1>
+      <button className="btn btn-primary" onClick={handleClick}>
+        Get All Blogs
+      </button>
     </div>
   );
 };
