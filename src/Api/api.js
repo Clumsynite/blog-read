828d940b30b337c941fb085408bd3694fb8f22fb
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-29 15:05:15 +0530

    refactored and added apiCall to getAllBlogs

--- src/Api/api.js
+++ src/Api/api.js
@@ -2,23 +2,23 @@ const apiUrl = "https://clumsy-blog.herokuapp.com";
 
 export const ping = async () => {
   try {
-    const res = await fetch(apiUrl)
-    return await res.json()    
+    const res = await fetch(apiUrl);
+    return await res.json();
   } catch (error) {
-    return error
+    return error;
   }
-}
+};
 
 export const userLogin = async (user) => {
   try {
     const response = await fetch(`${apiUrl}/auth/login`, {
-      method:"POST",
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(user)
+      body: JSON.stringify(user),
     });
-    const data = await response.json()
+    const data = await response.json();
     return data;
   } catch (error) {
     return error;
@@ -27,13 +27,27 @@ export const userLogin = async (user) => {
 
 export const userLogout = async () => {
   try {
-    const response = await fetch(`${apiUrl}/auth/logout`,
-    {
-      method: "POST"
-    })
-    const data = await response.json()
-    return data
+    const response = await fetch(`${apiUrl}/auth/logout`, {
+      method: "POST",
+    });
+    const data = await response.json();
+    return data;
   } catch (error) {
     return error;
   }
-}
+};
+
+export const getAllBlogs = async (token) => {
+  try {
+    const response = await fetch(`${apiUrl}/blog/all`, {
+      mode: "cors",
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
