49f3d6a5913821821b208dd5a77e287d2d00293c
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-29 17:21:13 +0530

    added loading animation on render

--- src/components/AllBlogs.js
+++ src/components/AllBlogs.js
@@ -1,8 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { useLoading, Bars } from "@agney/react-loading";
+
 import { getAllBlogs } from "../Api/api";
 import Error from "./Error";
 
 const AllBlogs = () => {
+  const [loading, setloading] = useState(true);
+  const { containerProps, indicatorEl } = useLoading({
+    loading,
+    indicator: <Bars width="100" />,
+    loaderProps: {
+      style: { color: "#00ffbf" },
+    },
+  });
   const [blogs, setblogs] = useState([]);
   useEffect(() => {
     const getBlogs = async () => {
@@ -10,7 +20,9 @@ const AllBlogs = () => {
         const token = localStorage.getItem("token");
         const blogs = await getAllBlogs(token);
         setblogs(blogs);
+        setloading(false);
       } catch (error) {
+        setloading(false);
         seterror(error);
       }
     };
@@ -30,12 +42,18 @@ const AllBlogs = () => {
           {fullname} AKA {author.username}
         </div>
         <div>{content}</div>
+        <div>{added}</div>
       </div>
     );
   });
 
   return (
     <div className="Blog">
+      {loading && (
+        <div className="text-center" {...containerProps}>
+          {indicatorEl}
+        </div>
+      )}
       {blogs.length > 0 && blogsMap}
       {error.length > 0 && <Error error={error} />}
     </div>
