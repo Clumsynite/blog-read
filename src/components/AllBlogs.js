bb067744d7e2869a37f79b02ed30ed792789c46e
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-30 19:49:37 +0530

    moved mapped render to BlogCard template

--- src/components/AllBlogs.js
+++ src/components/AllBlogs.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useLoading, Bars } from "@agney/react-loading";
 import { getAllBlogs } from "../scripts/api-calls";
+import BlogCard from "../Templates/BlogCard";
 import Error from "./Error";
-import { getRelativeTime, getContentPreview, getFullname } from "../scripts/helper";
 
 const AllBlogs = () => {
   const [loading, setloading] = useState(true);
@@ -32,31 +32,7 @@ const AllBlogs = () => {
   const [error, seterror] = useState("");
 
   const blogsMap = blogs.map((blog, index) => {
-    const { author, title, content, added } = blog;
-    return (
-      <div
-        key={index}
-        className="card text-white bg-primary shadow mb-4 bg-white rounded"
-      >
-        <div className="card-header text-center bg-dark">{title}</div>
-        <div className="card-body bg-light text-dark">
-          <div className="card-text ">{getContentPreview(content)}</div>
-        </div>
-        <div className="card-footer text-white bg-primary text-right d-flex justify-content-between flex-wrap">
-          <div className="d-flex align-items-center">
-            By {getFullname(author)}
-            <strong>
-              <span className="badge badge-pill badge-dark mx-1"> AKA </span>
-            </strong>
-            {author.username}
-          </div>
-          <div className="d-flex align-items-center">
-            <i className="material-icons mr-1">access_time</i>
-            {getRelativeTime(added)}
-          </div>
-        </div>
-      </div>
-    );
+    return <BlogCard blog={blog} key={index} />;
   });
 
   return (
