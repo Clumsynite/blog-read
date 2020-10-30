d8ae9b33c4c2f490cb5293b03a5c4452f0717f99
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-30 19:18:44 +0530

    added helper functions for fullname and contentPreview

--- src/scripts/helper.js
+++ src/scripts/helper.js
@@ -4,3 +4,23 @@ export const getRelativeTime = (time) => {
   const hours = moment(time).startOf("hour").fromNow();
   return hours;
 };
+
+export const getContentPreview = (content) => {
+  const words = content.split(" ");
+  if (words.length < 30) {
+    return content;
+  } else {
+    let preview;
+    for (let i = 0; i < words.length; i++) {
+      if (i >= 30) {
+        break;
+      }
+      preview += ` ${words[i]}`;
+    }
+    return `${preview} ...`;
+  }
+};
+
+export const getFullname = (author) => {
+  return `${author.firstname} ${author.lastname}`;
+};
