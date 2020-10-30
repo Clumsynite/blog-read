f6be88de01e94f456d9d3f7fad2bf28274e9ce31
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-30 20:18:58 +0530

    refactored
    comment in Profile for reference

--- src/Templates/BlogCard.js
+++ src/Templates/BlogCard.js
@@ -7,9 +7,7 @@ import {
 const Card = (props) => {
   const { author, title, content, added } = props.blog;
   return (
-    <div
-      className="card text-white bg-primary shadow mb-4 bg-white rounded"
-    >
+    <div className="card text-white bg-primary shadow mb-4 bg-white rounded">
       <div className="card-header text-center bg-dark">{title}</div>
       <div className="card-body bg-light text-dark">
         <div className="card-text ">{getContentPreview(content)}</div>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,40 @@
 import React, { useState, useEffect } from "react";
 import { myProfile } from "../scripts/api-calls";
+import { getFullname, getRelativeTime } from "../scripts/helper";
+
+/*
+// Blog - Array
+
+added: "2020-10-07T17:29:38.710Z"
+author: {_id: "5f7ddee74a0a7737982b2df7", firstname: "Testing", lastname: "API", username: "testac", password: "$2a$10$R8QdDs0TxPxnJwoGfjPOc.JR05Cj2qP0UE7doKHb3STj7ZjWIgye2", …}
+content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget dui est. Phasellus ut risus velit. Pellentesque sapien diam, ultrices eget turpis at, ultrices tincidunt est. Duis ac ligula rutrum, commodo enim sed, laoreet diam. Fusce gravida eros vel diam faucibus, vitae pretium velit sagittis. Suspendisse quis lorem risus. Pellentesque sit amet cursus erat. Integer ac ligula quis leo tempor mattis. Sed aliquam dolor libero, sed ultricies odio luctus a. Aenean molestie cursus nisi id fringilla. Nunc mi lacus, gravida eu sapien sed, consequat cursus velit."
+draft: false
+title: "Third Blog Title"
+__v: 0
+_id: "5f7dfb0248fa3c29ac
+
+// Comments - Array
+
+added: "2020-10-07T18:33:22.704Z"
+author: {_id: "5f7ddee74a0a7737982b2df7", firstname: "Testing", lastname: "API", username: "testac", password: "$2a$10$R8QdDs0TxPxnJwoGfjPOc.JR05Cj2qP0UE7doKHb3STj7ZjWIgye2", …}
+blog: null
+content: "Testing whether comment's author can update the comment"
+title: "Testing Comment Update"
+__v: 0
+_id: "5f7e09f21a773b25e
+
+// User - Object
+
+user:
+added: "2020-10-08T11:23:09.439Z"
+firstname: "Testing"
+lastname: "API"
+password: "$2a$10$R8QdDs0TxPxnJwoGfjPOc.JR05Cj2qP0UE7doKHb3STj7ZjWIgye2"
+username: "testac"
+__v: 0
+_id: "5f7ddee74a0a7737982b
+
+*/
 
 const Profile = () => {
   const [profile, setprofile] = useState([]);
