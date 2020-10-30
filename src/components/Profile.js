fa628e1dcec62cf8a21df0ecdcceb08a92f5cce7
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-30 15:59:55 +0530

    modfied profile component to get useProfile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { myProfile } from "../Api/api";
+
 const Profile = () => {
+  const [profile, setprofile] = useState([]);
+  const token = localStorage.getItem("token");
+  // useEffect(() => {
+  //   const getProfile = async () => {
+  //     try {
+  //       const data = await myProfile(token);
+  //       console.log(data);
+  //       setprofile(data);
+  //     } catch (error) {}
+  //   };
+  //   // getProfile();
+  //   return profile;
+  // }, []);
+  const getProfile = async () => {
+    try {
+      const data = await myProfile(token);
+      console.log(data);
+      setprofile(data);
+    } catch (error) {
+      console.error(error)
+    }
+  };
   return (
     <div className="Profile">
       <h1 className="text-center">Profile</h1>
+      <button className="btn btn-block btn-primary" onClick={getProfile}>
+        {" "}
+        Get Profile
+      </button>
     </div>
   );
 };
