11738685127164826c9847bd263691d1917deca2
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-30 21:27:53 +0530

    added loading animation

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from "react";
 import BlogCard from "../Templates/BlogCard";
+import { useLoading, Bars } from "@agney/react-loading";
 import { myProfile } from "../scripts/api-calls";
 import { getFullname, getRelativeTime } from "../scripts/helper";
 
 const Profile = () => {
+  const [loading, setloading] = useState(true);
+  const { containerProps, indicatorEl } = useLoading({
+    loading,
+    indicator: <Bars width="100" />,
+    loaderProps: {
+      style: { color: "#17A2B8" },
+    },
+  });
   const [profile, setprofile] = useState([]);
   const [render, setRender] = useState();
   const [user, setUser] = useState({});
@@ -16,8 +25,10 @@ const Profile = () => {
         setprofile(data);
         setUser(data.user);
         setRender("blogs");
+        setloading(false);
       } catch (error) {
         console.error(error);
+        setloading(false);
       }
     };
     getProfile();
@@ -25,9 +36,16 @@ const Profile = () => {
 
   return (
     <div className="Profile">
+      {loading && (
+        <div className="text-center" {...containerProps}>
+          {indicatorEl}
+        </div>
+      )}
       {profile.user && (
         <div className="card mb-3 mx-auto" style={{ width: "24rem" }}>
-          <div className="card-header bg-dark text-white">Joined {getRelativeTime(user.added)}</div>
+          <div className="card-header bg-dark text-white">
+            Joined {getRelativeTime(user.added)}
+          </div>
           <div className="card-body bg-light">
             {getFullname(user)}{" "}
             <strong>
