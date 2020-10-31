0a0ec3057be0ef3964a1a74d04eea2a59432a71b
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-31 16:58:15 +0530

    removed link and blog data from comment card

--- src/Templates/CommentCard.js
+++ src/Templates/CommentCard.js
@@ -1,20 +1,14 @@
-import { Link } from "react-router-dom";
 import { getRelativeTime, getFullname } from "../scripts/helper";
 import "../styles/BlogCard.css";
 
 const Card = (props) => {
-  const { author, title, content, added, blog } = props.comment;
+  const { author, title, content, added } = props.comment;
   return (
-    <div className="card shadow rounder mb-5">
-      <Link to={`/blog/${blog._id}/view`} className="link">
-        <div className="card-header text-center bg-dark text-light align-items-center">
-          You commented on <code>{blog.title}</code>
-        </div>
-        <div className="card-body bg-light text-dark">
-          <h4 className="card-title">{title}</h4>
-          <div className="card-text ">{content}</div>
-        </div>
-      </Link>
+    <div className="card shadow rounder mb-3">
+      <div className="card-body bg-light text-dark">
+        <h4 className="card-title">{title}</h4>
+        <div className="card-text ">{content}</div>
+      </div>
       <div className="card-footer text-white bg-secondary text-right d-flex justify-content-between flex-wrap">
         <div className="d-flex align-items-center">
           <i className="material-icons mr-1">account_circle</i>
