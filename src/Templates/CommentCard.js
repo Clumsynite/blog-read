cefc88916b603390f9031dbd16d03fca668cb6fc
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-31 15:37:51 +0530

    Modifed comments to render link to their respective blog posts

--- src/Templates/CommentCard.js
+++ src/Templates/CommentCard.js
@@ -6,13 +6,18 @@ const Card = (props) => {
   const { author, title, content, added, blog } = props.comment;
   return (
     <div className="card shadow rounder mb-5">
-      <div className="card-header text-center bg-dark text-light">{title}</div>
-      <div className="card-body bg-light text-dark">
-        <div className="card-text ">{content}</div>
-      </div>
+      <Link to={`/blog/${blog._id}/view`} className="link">
+        <div className="card-header text-center bg-dark text-light align-items-center">
+          You commented on <code>{blog.title}</code>
+        </div>
+        <div className="card-body bg-light text-dark">
+          <h4 className="card-title">{title}</h4>
+          <div className="card-text ">{content}</div>
+        </div>
+      </Link>
       <div className="card-footer text-white bg-secondary text-right d-flex justify-content-between flex-wrap">
         <div className="d-flex align-items-center">
-          <i className="material-icons mr-1">account_circle</i>{" "}
+          <i className="material-icons mr-1">account_circle</i>
           {getFullname(author)}
           <strong>
             <span className="badge badge-pill badge-dark mx-1"> AKA </span>
