1436800bf08481f44ea9c7fa2c6bda3674bb0dba
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-31 13:26:32 +0530

    modified to render blog post

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useLoading, Oval } from "@agney/react-loading";
-import { viewBlog } from "../scripts/api-calls";
 import Error from "./Error";
+import { viewBlog } from "../scripts/api-calls";
+import { getRelativeTime, getFullname } from "../scripts/helper";
 
 const BlogPost = () => {
   const { id } = useParams();
@@ -25,7 +26,7 @@ const BlogPost = () => {
         if (data.error) {
           setloading(false);
           seterror(
-            `Blog Post not found. There's a Problem fetching Post:${id}`
+            `Blog Post not found. There's a Problem fetching Post: ${id}`
           );
           return;
         }
@@ -42,7 +43,6 @@ const BlogPost = () => {
 
   return (
     <div>
-      <h3>ID: {id} </h3>
       {loading && (
         <div className="text-center my-5" {...containerProps}>
           {indicatorEl}
@@ -51,6 +51,28 @@ const BlogPost = () => {
       {error.length > 0 > 0 && (
         <div className="w-75 mx-auto">
           <Error error={error} />
+        </div>
+      )}
+      {post.title && !loading && (
+        <div className="card shadow rounded">
+          <div className="card-body">
+            <h1 className="card-title text-center">{post.title}</h1>
+            <div className="card-subtitle mb-2 text-muted d-flex justify-content-between flex-wrap">
+              <div className="d-flex align-items-center">
+                <i className="material-icons mr-1">account_circle</i>
+                {getFullname(post.author)}
+                <strong>
+                  <span className="badge badge-pill badge-dark mx-1">AKA</span>
+                </strong>
+                {post.author.username}
+              </div>
+              <div className="d-flex align-items-center">
+                <i className="material-icons mr-1">access_time</i>
+                {getRelativeTime(post.added)}
+              </div>
+            </div>
+            <div className="card-text">{post.content}</div>
+          </div>
         </div>
       )}
     </div>
