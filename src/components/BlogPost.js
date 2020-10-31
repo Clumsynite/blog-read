52ac652370c652bc9283bfa47e2a0a2db0667b5c
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-31 09:10:00 +0530

    updated BlogPost to read url Parameters

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const BlogPost = () => {
-
-  return(
+  const { id } = useParams();
+  return (
     <div>
-
+      <h3>ID: {id} </h3>
     </div>
-  )
-}
+  );
+};
 
-export default BlogPost
+export default BlogPost;
