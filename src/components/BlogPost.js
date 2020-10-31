fd53bc6554b92e7a75f817d478c002acf5d70894
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-31 09:24:04 +0530

    modified to fetch blogpost

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,8 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { viewBlog } from "../scripts/api-calls";
 
 const BlogPost = () => {
   const { id } = useParams();
+  const [post, setPost] = useState({});
+  const [comments, setComments] = useState([]);
+  const [error, seterror] = useState("");
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const fetchPost = async () => {
+      try {
+        const data = await viewBlog(id, token);
+        console.log(data);
+        setPost(data.blog);
+        setComments(data.comment);
+      } catch (error) {
+        console.error(error);
+        seterror(error);
+      }
+    };
+    fetchPost();
+  }, [id]);
+
   return (
     <div>
       <h3>ID: {id} </h3>
