b351f5126707ef698b2e1e31073b1a5e33bbc2e4
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-31 14:37:26 +0530

    rendered each comment as cards

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -97,6 +97,38 @@ const Profile = () => {
           You haven't made any comments yet
         </div>
       )}
+      {render === "comments" &&
+        profile.comments.length > 0 &&
+        profile.comments.map((comment, index) => {
+          const { title, content, author, blog, added } = comment;
+          return (
+            <div className="card shadow rounder mb-5" key={index}>
+              <div className="card-header text-center bg-dark text-light">
+                {title}
+              </div>
+              <div className="card-body bg-light text-dark">
+                <div className="card-text ">{content}</div>
+              </div>
+              <div className="card-footer text-white bg-secondary text-right d-flex justify-content-between flex-wrap">
+                <div className="d-flex align-items-center">
+                  <i className="material-icons mr-1">account_circle</i>{" "}
+                  {getFullname(author)}
+                  <strong>
+                    <span className="badge badge-pill badge-dark mx-1">
+                      {" "}
+                      AKA{" "}
+                    </span>
+                  </strong>
+                  {author.username}
+                </div>
+                <div className="d-flex align-items-center">
+                  <i className="material-icons mr-1">access_time</i>
+                  {getRelativeTime(added)}
+                </div>
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 };
