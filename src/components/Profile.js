4ff111c12bf25eb0191f3c5ab5fda1d696c9f630
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-31 14:49:39 +0530

    used CommentCard component instead

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import BlogCard from "../Templates/BlogCard";
+import CommentCard from "../Templates/CommentCard";
 import { useLoading, Bars } from "@agney/react-loading";
 import { myProfile } from "../scripts/api-calls";
 import { getFullname, getRelativeTime } from "../scripts/helper";
@@ -100,34 +101,7 @@ const Profile = () => {
       {render === "comments" &&
         profile.comments.length > 0 &&
         profile.comments.map((comment, index) => {
-          const { title, content, author, blog, added } = comment;
-          return (
-            <div className="card shadow rounder mb-5" key={index}>
-              <div className="card-header text-center bg-dark text-light">
-                {title}
-              </div>
-              <div className="card-body bg-light text-dark">
-                <div className="card-text ">{content}</div>
-              </div>
-              <div className="card-footer text-white bg-secondary text-right d-flex justify-content-between flex-wrap">
-                <div className="d-flex align-items-center">
-                  <i className="material-icons mr-1">account_circle</i>{" "}
-                  {getFullname(author)}
-                  <strong>
-                    <span className="badge badge-pill badge-dark mx-1">
-                      {" "}
-                      AKA{" "}
-                    </span>
-                  </strong>
-                  {author.username}
-                </div>
-                <div className="d-flex align-items-center">
-                  <i className="material-icons mr-1">access_time</i>
-                  {getRelativeTime(added)}
-                </div>
-              </div>
-            </div>
-          );
+          return <CommentCard comment={comment} key={index} />;
         })}
     </div>
   );
